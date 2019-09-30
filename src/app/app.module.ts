63c05edec880f6c4653e340a1bbeec268a81316a
Tyler Cronin <tyler_cronin@yahoo.com>
2019-09-30 14:19:39 -0400

    Added some rain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestingComponent } from './testing/testing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingComponent
   ],
   imports: [
     BrowserModule,
